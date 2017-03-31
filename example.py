412d7c4b8920f082bd08b8904333c504ccbd7914
Marcos Oliveira <moliveirajun2013@my.fit.edu>
2017-03-31 17:26:31 -0400

    fitting crime

--- example.py
+++ example.py
@@ -14,7 +14,7 @@ models = {'lognormal_delta_fixed': analysis.LogNormalFixedDAnalysis,
           'gaussian_beta_fixed': analysis.ConstrainedDFixedBetaAnalysis,
           'person': analysis.PopulationAnalysis}
 
-df_15 = pd.read_excel("/home/marcos/Downloads/Table_8_Offenses_Known_to_Law_Enforcement_by_State_by_City_2015.xls", "15tbl08")
+df_15 = pd.read_excel("~/Table_8_Offenses_Known_to_Law_Enforcement_by_State_by_City_2015.xls", "15tbl08")
 states = list(df_15.State)
 states_index = [k for k in range(len(states)) if type(states[k]) == unicode]
 states_correction = list(np.concatenate([[states[states_index[i]]]*(states_index[i+1] - states_index[i]) for i in range(len(states_index) - 1)]))
@@ -26,31 +26,29 @@ types = ["Larceny-\ntheft", "Burglary", "Robbery", "Violent\ncrime", "Rape\n(leg
 
 for typec in types:
     print typec
-    df_15_ = df_15[df_15['Population'] > 10000]
+    df_15_ = df_15[df_15['Population'] > 10]
     df_15_ = df_15_[df_15_[typec] > 1]
     x = np.array(map(float, df_15_['Population']))
     y = np.array(map(float, df_15_[typec]))
     x = x[~pd.isnull(y)]
     y = y[~pd.isnull(y)]
     data_xy = x, y
-    for m in models:
+    for m, model in models.items():
         line = typec.replace("\n", " ") + ","
-        r = m(data_xy, required_successes=1)
-        line += m.description + ", "
-        line += ",".join(r.beta) + ", "
-        line += r.bic + ", "
-        line += r.p_value + ", "
+        r = model(data_xy, required_successes=8)
+        line += "%s," % m.description
+        line += ",".join(map(str, r.beta)) + ","
+        line += "%f," % r.bic
+        line += "%f" % r.p_value
         print line
 
-
-
-df_14 = pd.read_excel("/home/marcos/Downloads/table-8.xls", "14tbl08")
-df_14 = df_14[~pd.isnull(df_14['City'])]
-states = list(df_14.State)
-states_index = [k for k in range(len(states)) if type(states[k]) == unicode]
-states_correction = list(np.concatenate([[states[states_index[i]]]*(states_index[i+1] - states_index[i]) for i in range(len(states_index) - 1)]))
-states_correction += [states[states_index[-1]]]*(len(states)-states_index[-1])
-df_14['State'] = states_correction
-df_14['City'] = map(lambda x: re.sub("[0-9]*", "", x), df_14.City)
-df_2LA = df_15.merge(df_14, how="left", on=["State", "City"])
-df_2LA.columns
+# df_14 = pd.read_excel("/home/marcos/Downloads/table-8.xls", "14tbl08")
+# df_14 = df_14[~pd.isnull(df_14['City'])]
+# states = list(df_14.State)
+# states_index = [k for k in range(len(states)) if type(states[k]) == unicode]
+# states_correction = list(np.concatenate([[states[states_index[i]]]*(states_index[i+1] - states_index[i]) for i in range(len(states_index) - 1)]))
+# states_correction += [states[states_index[-1]]]*(len(states)-states_index[-1])
+# df_14['State'] = states_correction
+# df_14['City'] = map(lambda x: re.sub("[0-9]*", "", x), df_14.City)
+# df_2LA = df_15.merge(df_14, how="left", on=["State", "City"])
+# df_2LA.columns
